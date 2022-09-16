2490904aa00c7d7d3ee9c82bee69149bc3f2e45f
Vi-Anh Nguyen <41245033+bizbunny@users.noreply.github.com>
2022-09-16 11:01:17 -0500

    updated some minor details in log

--- hw1/hw1.js
+++ hw1/hw1.js
@@ -151,7 +151,14 @@ const Product = class Product {
      * Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat (currency formatting)
      */
     static printProducts = (products) => {
-        console.log(`Product: ${products.name}, Cost: ${new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD' }).format(products.price)}, Available: Yes`);
+        for(let i of products){
+            if(i.availability === 'In Stock'){
+                console.log(`Product: ${i.name}, Cost: ${new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD' }).format(parseFloat(i.price))}, Availability: Yes`);
+            }
+            else if(i.availability === 'Out of Stock'){
+                console.log(`Product: ${i.name}, Cost: ${new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD' }).format(parseFloat(i.price))}, Availability: No`);
+            }
+        }
     }
 
  };
